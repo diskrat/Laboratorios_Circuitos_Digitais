1190f80d6fb8b8b6f936254a1f988d942342acea
Joao Silva <joaodoer3@gmail.com>
2023-08-25 09:23:01 -0300

    adicionado comentarios descritivos

--- Laboratorio 1 Portas Logicas/and_or_postulados/and_or_postulados.vhdl
+++ Laboratorio 1 Portas Logicas/and_or_postulados/and_or_postulados.vhdl
@@ -16,21 +16,37 @@ end;
 
 architecture arch of and_or_postulados is
 begin
+
+    -- And gate
     gate_and <= a and b and c;
+    
+    -- Or gate
     gate_or <= a or b or c;
+    
+    -- Identidades
     Identidade_or <= a or '0';
     Identidade_and <= a and '1';
     Identidade_not <= not (not a);
+    
+    -- Adição
     adicao_1 <= a or (b or c);
     adicao_2 <= (a or b) or c;
+    
+    -- Multiplicacao
     multiplicacao_1 <= a and (b and c);
     multiplicacao_2 <= (a and b) and c;
+    
+    -- Distributividade
     distributiva_and_1 <= a and (b or c);
     distributiva_and_2 <= (a and b) or (a and c);
     distributiva_or_1 <= a or (b and c);
     distributiva_or_2 <= (a or b) and (a or c);
+    
+    -- Primeira lei de Morgan
     primeira_morgan_1 <= not (a and b);
     primeira_morgan_2 <= not a or not b;
+    
+    -- Segunda lei de Morgan
     segunda_morgan_1 <= not (a or b);
     segunda_morgan_2 <= not a and not b;
     
