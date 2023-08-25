9b258d396af37a89546fc72b388c11b19b5e139c
Joao Silva <joaodoer3@gmail.com>
2023-08-25 10:31:55 -0300

    adicao de coisas esquecidas

--- Laboratorio 1 Portas Logicas/and_or_postulados/and_or_postulados.vhdl
+++ Laboratorio 1 Portas Logicas/and_or_postulados/and_or_postulados.vhdl
@@ -5,6 +5,7 @@ entity and_or_postulados is
     port( 
         a,b,c : in std_logic;
         gate_and,gate_or,
+        Comutativa_or_1,Comutativa_or_2, Comutativa_and_1,Comutativa_and_2,
         Identidade_or,Identidade_and,Identidade_not,
         adicao_1,adicao_2,multiplicacao_1,multiplicacao_2,
         distributiva_and_1,distributiva_and_2,
@@ -22,6 +23,12 @@ begin
     
     -- Or gate
     gate_or <= a or b or c;
+    
+    -- Comutatividade
+    Comutativa_or_1 <= a or b;
+    Comutativa_or_2 <= b or a; 
+    Comutativa_and_1 <= a and b;
+    Comutativa_and_2 <= b and a;
     
     -- Identidades
     Identidade_or <= a or '0';
